a717ffc5e66daf40fedafbe68df1ea86cffc0cb4
ratul-islam <ratulislam20502020@gmail.com>
2023-05-22 00:58:01 +0600

    changed result from string to array

--- src/components/converttext/page.jsx
+++ src/components/converttext/page.jsx
@@ -1,12 +1,11 @@
 "use client"
 
-import React, {useState, useRef} from 'react'
+import React, {useState} from 'react'
 
 import styles from './styles.module.css'
 
 const GetData = () => {
-    const buttonRef = useRef(null);
-
+    const [semesterName, setSemesterName] = useState('')
     const [rawData, setRawData] = useState('')
     const [rolls, setRolls] = useState()
     const [finalData, setFinalData] = useState([])
@@ -16,14 +15,11 @@ const handleChange = (e) =>{
     setRolls(e.target.value.match(/\d{6}/g))
 
 }
+const handleNameChange = (e) =>{
+    setSemesterName(e.target.value)
+}
 const handleSubmit =(event)=> {
     event.preventDefault();
-    
-    
-
-    
-
-
 }
 
 const handlebtn = (e) =>{
@@ -33,7 +29,9 @@ const handlebtn = (e) =>{
            
            let info = {
                roll: rolls[i],
-               result: rawData.slice(rawData.indexOf(rolls[i])+8 , rawData.indexOf(rolls[i])+12),
+               result: {
+                semesterName : rawData.slice(rawData.indexOf(rolls[i])+8 , rawData.indexOf(rolls[i])+12)
+            },
                passed: true,
            }
            setFinalData((oldData) => [...oldData , {info}])
@@ -48,7 +46,7 @@ const handlebtn = (e) =>{
    
                        let info = {
                            roll: rolls[i],
-                           result: rawData.slice(rawData.indexOf(rolls[i])+7,start+1),
+                           result: {semesterName: rawData.slice(rawData.indexOf(rolls[i])+7,start+1)},
                            passed: false,
                        }
                        setFinalData((oldData) => [...oldData , {info}])
@@ -58,20 +56,24 @@ const handlebtn = (e) =>{
    
            }
        }
-       console.log(finalData)
+       
+       e.target.setAttribute('disabled', 'disabled')
+       // e.target.styles.backgroundColor = 'gray';
+    }
+//     const consoleValue = () =>{
+//     console.log(finalData)
     
-        e.target.setAttribute('disabled', 'disabled')
-        // e.target.styles.backgroundColor = 'gray';
-}
-
+// }
 
   return (
     <main>
         <div>
         <form method='POST' className={styles.form} onSubmit={handleSubmit}>
+            <input className={styles.input} onChange={handleNameChange} type='text' name='semester' />
             <textarea className={styles.textArea} onChange={handleChange} placeholder='Copy the result pdf text ad past it in here'/>
             <input className={styles.btn} type='submit'  onClick={handlebtn} value='Get Data' />
         </form>
+            {/* <input className={styles.btn} type='submit'  onClick={consoleValue} value='console log' /> */}
 
     </div>
     <div className={styles.wrapper}>
@@ -79,7 +81,7 @@ const handlebtn = (e) =>{
             return (
                 <div key={roll.info.roll} id={roll.info.roll}>
                 <h2>{roll.info.roll}</h2>
-                <p>{roll.info.result}</p>
+                <p>{roll.info.result[0]}</p>
             </div>
             )
         })}
